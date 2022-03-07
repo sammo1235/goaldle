f6970da9bb977dc9f7d1c7c588bc3cca9f60d031
sammo <samrhysperry93@hotmail.co.uk>
2022-03-07 19:31:02 +0000

    iOS copy/share results

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -250,7 +250,47 @@ export default {
         document.execCommand('copy');
         document.removeEventListener('copy', listener);
       }
-      copyRichText(str);
+
+      if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+        let result;
+        let textarea;
+        try {
+          textarea = document.createElement('textarea');
+          textarea.setAttribute('readonly', true);
+          textarea.setAttribute('contenteditable', true);
+          textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+          textarea.value = str;
+          textarea.focus();
+          textarea.select();
+
+          const range = document.createRange();
+          range.selectNodeContents(textarea);
+
+          const sel = window.getSelection();
+          sel.removeAllRanges();
+          sel.addRange(range);
+
+          textarea.setSelectionRange(0, textarea.value.length);
+          result = document.execCommand('copy');
+        } catch (err) {
+          console.error(err);
+          result = null;
+        } finally {
+          document.body.removeChild(textarea);
+        }
+        // manual copy fallback using prompt
+        if (!result) {
+          const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+          const copyHotkey = isMac ? '⌘C' : 'CTRL+C';
+          result = prompt(`Press ${copyHotkey}`, str); // eslint-disable-line no-alert
+          if (!result) {
+            return false;
+          }
+        }
+        return true;
+      } else {
+        copyRichText(str);
+      }
       document.getElementById('copybtn').innerHTML = "Copied!"
     }
   }
